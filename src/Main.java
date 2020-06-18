f479d8d59092d34ceb058daa5e4e846e06a7e5ef
MrNukeBlast <bnikolayev993@gmail.com>
2020-06-18 07:58:33 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int [][] arr = new int[3][3];
+
+
+
+    }
+
+    public static void array (int a, int b){
+
+
+    }
+
+}
